f0f314c9b31b1516b43c73b42ab7bd1bb8f7989b
Vitor Quadros <viitorquadros@hotmail.com>
2022-08-04 10:22:31 -0300

    update gitignore

---
+++ avaliacao-2/src/models/Conta.java
@@ -0,0 +1,14 @@
+package models;
+
+public abstract class Conta {
+
+    private double saldo;
+
+    public void deposita(double valor) {
+
+    }
+
+    public void saca(double valor) {}
+
+    public void atualiza(double taxa) {}
+}
