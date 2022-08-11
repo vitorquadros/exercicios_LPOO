ee840024554c29e9241568ac3d6493954af3ba21
Vitor Quadros <viitorquadros@hotmail.com>
2022-08-11 10:05:27 -0300

    feat (avaliacao 2): implement Conta methods

--- avaliacao-2/src/models/Conta.java
+++ avaliacao-2/src/models/Conta.java
@@ -2,15 +2,17 @@ package models;
 
 public abstract class Conta {
 
-    private double saldo;
+    protected double saldo;
 
     public void deposita(double valor) {
-
+        this.saldo += valor;
     }
 
     public void saca(double valor) {
+        this.saldo -= valor;
     }
 
     public void atualiza(double taxa) {
+        this.saldo = this.saldo * (taxa / 100);
     }
 }
