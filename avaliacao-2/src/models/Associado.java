195b645442de2154b5318bb783b49f06491a5f08
Vitor Quadros <viitorquadros@hotmail.com>
2022-08-11 10:18:11 -0300

    feat (avaliacao 2): implement constructor, getter and toString on Associado

--- avaliacao-2/src/models/Associado.java
+++ avaliacao-2/src/models/Associado.java
@@ -1,10 +1,25 @@
 package models;
 
 public class Associado implements AssociadoVip {
-    private String nome;
+    private final String nome;
+
+    public Associado(String nome) {
+        this.nome = nome;
+    }
 
     @Override
     public double lucros(int qtdCotas, double valorCota) {
         return 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Associado{" +
+                "nome='" + nome + '\'' +
+                '}';
     }
 }
